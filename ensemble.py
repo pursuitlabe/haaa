94357a859bd76ffa2800db7572aac07dce5687ae
pursuitlabe <3049412151@qq.com>
2024-10-27 22:48:22 +0800

    更改

--- ensemble.py
+++ ensemble.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset',
                         required=True,
-                        choices={'ntu/xsub', 'ntu/xview', 'ntu120/xsub', 'ntu120/xset', 'NW-UCLA'},
+                        choices={'ntu/xsub', 'ntu/xview', 'ntu120/xsub', 'ntu120/xset', 'NW-UCLA', 'uav'},
                         help='the work folder for storing results')
     parser.add_argument('--alpha',
                         default=1,
@@ -47,68 +47,30 @@ if __name__ == "__main__":
         elif 'xview' in arg.dataset:
             npz_data = np.load('./data/' + 'ntu/' + 'NTU60_CV.npz')
             label = np.where(npz_data['y_test'] > 0)[1]
+    elif 'uav' in arg.dataset:
+        label = np.load('./data/' + 'uav/' + 'test_label_B.npy')
     else:
         raise NotImplementedError
 
-    with open(os.path.join(arg.joint_dir, 'epoch1_test_score.pkl'), 'rb') as r1:
-        r1 = list(pickle.load(r1).items())
+    r1 = list(np.load(os.path.join(arg.joint_dir, 'epoch1_test_score.npy'), allow_pickle=True))
 
-    with open(os.path.join(arg.bone_dir, 'epoch1_test_score.pkl'), 'rb') as r2:
-        r2 = list(pickle.load(r2).items())
-
-    if arg.joint_motion_dir is not None:
-        with open(os.path.join(arg.joint_motion_dir, 'epoch1_test_score.pkl'), 'rb') as r3:
-            r3 = list(pickle.load(r3).items())
-    if arg.bone_motion_dir is not None:
-        with open(os.path.join(arg.bone_motion_dir, 'epoch1_test_score.pkl'), 'rb') as r4:
-            r4 = list(pickle.load(r4).items())
+    r2 = list(np.load(os.path.join(arg.bone_dir, 'epoch1_test_score.npy'), allow_pickle=True))
 
     right_num = total_num = right_num_5 = 0
 
-    if arg.joint_motion_dir is not None and arg.bone_motion_dir is not None:
-        arg.alpha = [0.6, 0.6, 0.4, 0.4]
-        for i in tqdm(range(len(label))):
-            l = label[i]
-            _, r11 = r1[i]
-            _, r22 = r2[i]
-            _, r33 = r3[i]
-            _, r44 = r4[i]
-            r = r11 * arg.alpha[0] + r22 * arg.alpha[1] + r33 * arg.alpha[2] + r44 * arg.alpha[3]
-            rank_5 = r.argsort()[-5:]
-            right_num_5 += int(int(l) in rank_5)
-            r = np.argmax(r)
-            right_num += int(r == int(l))
-            total_num += 1
-        acc = right_num / total_num
-        acc5 = right_num_5 / total_num
-    elif arg.joint_motion_dir is not None and arg.bone_motion_dir is None:
-        arg.alpha = [0.6, 0.6, 0.4]
-        for i in tqdm(range(len(label))):
-            l = label[:, i]
-            _, r11 = r1[i]
-            _, r22 = r2[i]
-            _, r33 = r3[i]
-            r = r11 * arg.alpha[0] + r22 * arg.alpha[1] + r33 * arg.alpha[2]
-            rank_5 = r.argsort()[-5:]
-            right_num_5 += int(int(l) in rank_5)
-            r = np.argmax(r)
-            right_num += int(r == int(l))
-            total_num += 1
-        acc = right_num / total_num
-        acc5 = right_num_5 / total_num
-    else:
-        for i in tqdm(range(len(label))):
-            l = label[i]
-            _, r11 = r1[i]
-            _, r22 = r2[i]
-            r = r11 + r22 * arg.alpha
-            rank_5 = r.argsort()[-5:]
-            right_num_5 += int(int(l) in rank_5)
-            r = np.argmax(r)
-            right_num += int(r == int(l))
-            total_num += 1
+    for i in tqdm(range(len(label))):
+        l = label[i]
+        _, r11 = r1[i]
+        _, r22 = r2[i]
+        r = r11 + r22 * arg.alpha
+        rank_5 = r.argsort()[-5:]
+        right_num_5 += int(int(l) in rank_5)
+        r = np.argmax(r)
+        right_num += int(r == int(l))
+        total_num += 1
         acc = right_num / total_num
         acc5 = right_num_5 / total_num
 
+    np.save(r'pred.npy', np.array(r))
     print('Top1 Acc: {:.4f}%'.format(acc * 100))
     print('Top5 Acc: {:.4f}%'.format(acc5 * 100))
