d3057e5ca1dff820de8a698a792a94f52e97c0cd
pursuitlabe <134537765+pursuitlabe@users.noreply.github.com>
2024-10-28 10:51:43 +0800

    Update ensemble.py

--- ensemble.py
+++ ensemble.py
@@ -1,76 +1,13 @@
-import argparse
-import pickle
-import os
-
 import numpy as np
-from tqdm import tqdm
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset',
-                        required=True,
-                        choices={'ntu/xsub', 'ntu/xview', 'ntu120/xsub', 'ntu120/xset', 'NW-UCLA', 'uav'},
-                        help='the work folder for storing results')
-    parser.add_argument('--alpha',
-                        default=1,
-                        help='weighted summation',
-                        type=float)
-
-    parser.add_argument('--joint-dir',
-                        help='Directory containing "epoch1_test_score.pkl" for joint eval results')
-    parser.add_argument('--bone-dir',
-                        help='Directory containing "epoch1_test_score.pkl" for bone eval results')
-    parser.add_argument('--joint-motion-dir', default=None)
-    parser.add_argument('--bone-motion-dir', default=None)
-
-    arg = parser.parse_args()
-
-    dataset = arg.dataset
-    if 'UCLA' in arg.dataset:
-        label = []
-        with open('./data/' + 'NW-UCLA/' + '/val_label.pkl', 'rb') as f:
-            data_info = pickle.load(f)
-            for index in range(len(data_info)):
-                info = data_info[index]
-                label.append(int(info['label']) - 1)
-    elif 'ntu120' in arg.dataset:
-        if 'xsub' in arg.dataset:
-            npz_data = np.load('./data/' + 'ntu120/' + 'NTU120_CSub.npz')
-            label = np.where(npz_data['y_test'] > 0)[1]
-        elif 'xset' in arg.dataset:
-            npz_data = np.load('./data/' + 'ntu120/' + 'NTU120_CSet.npz')
-            label = np.where(npz_data['y_test'] > 0)[1]
-    elif 'ntu' in arg.dataset:
-        if 'xsub' in arg.dataset:
-            npz_data = np.load('./data/' + 'ntu/' + 'NTU60_CS.npz')
-            label = np.where(npz_data['y_test'] > 0)[1]
-        elif 'xview' in arg.dataset:
-            npz_data = np.load('./data/' + 'ntu/' + 'NTU60_CV.npz')
-            label = np.where(npz_data['y_test'] > 0)[1]
-    elif 'uav' in arg.dataset:
-        label = np.load('./data/' + 'uav/' + 'test_label_B.npy')
-    else:
-        raise NotImplementedError
-
-    r1 = list(np.load(os.path.join(arg.joint_dir, 'epoch1_test_score.npy'), allow_pickle=True))
-
-    r2 = list(np.load(os.path.join(arg.bone_dir, 'epoch1_test_score.npy'), allow_pickle=True))
-
-    right_num = total_num = right_num_5 = 0
-
-    for i in tqdm(range(len(label))):
-        l = label[i]
-        _, r11 = r1[i]
-        _, r22 = r2[i]
-        r = r11 + r22 * arg.alpha
-        rank_5 = r.argsort()[-5:]
-        right_num_5 += int(int(l) in rank_5)
-        r = np.argmax(r)
-        right_num += int(r == int(l))
-        total_num += 1
-        acc = right_num / total_num
-        acc5 = right_num_5 / total_num
-
-    np.save(r'pred.npy', np.array(r))
-    print('Top1 Acc: {:.4f}%'.format(acc * 100))
-    print('Top5 Acc: {:.4f}%'.format(acc5 * 100))
+# 加载模态数据
+modality1 = np.load(r'work_dir/uav/csub/bone_test/epoch1_test_score.npy') 
+modality2 = np.load(r'work_dir/uav/csub/joint/epoch1_test_score.npy')  
+# 如果有更多模态，继续加载
+# 设定权重
+weights = [0.5, 0.5]  # 模态1权重0.5，模态2权重0.5
+# 计算加权平均
+fused_output = weights[0] * modality1 + weights[1] * modality2
+# 保存结果
+np.save('pred.npy', fused_output)
+print("融合完成，置信度文件已保存为 'pred.npy'")
